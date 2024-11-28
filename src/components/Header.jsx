8ad4dd935fd480d702b0ceadb44a2334fea43809
Ahmed Belabdia <ab.belabdia@gmail.com>
2024-11-28 21:20:32 +0100

    put navlinks on header to navigate smoothly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,14 @@ export function Header() {
 
   return (
     <Navbar fluid className="fixed top-0 w-full z-50 bg-white shadow-md">
-      <Navbar.Brand href="/">
-        <img src="./fairprice_logo.svg" className="mr-3 h-6 sm:h-9" alt="FairPrice Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold italic dark:text-white">FairPrice</span>
-      </Navbar.Brand>
+
+      <NavLink to="/">
+        <div className="flex md:order-1">
+          <img src="./fairprice_logo.svg" className="mr-3 h-6 sm:h-9" alt="FairPrice Logo" />
+          <span className="self-center whitespace-nowrap text-xl font-semibold italic dark:text-white">FairPrice</span>
+        </div>
+      </NavLink>
+
       <div className="flex md:order-2">
         {!isLoggedIn && (
           <NavLink to="/login">
@@ -30,10 +34,21 @@ export function Header() {
 
         <Navbar.Toggle />
       </div>
+
       <Navbar.Collapse>
-        {isLoggedIn && (<Navbar.Link style={{ color: 'var(--secondary-color)' }} href="/products" active>My products</Navbar.Link>)}
-        {isLoggedIn && (<Navbar.Link style={{ color: 'var(--secondary-color)' }} href="/costs" active>My costs</Navbar.Link>)}
-        <Navbar.Link style={{ color: 'var(--secondary-color' }} href="/about" active>About</Navbar.Link>
+        {isLoggedIn && (
+          <NavLink to="/products">
+            <Navbar.Link style={{ color: 'var(--secondary-color)' }} active>My products</Navbar.Link>
+          </NavLink>
+        )}
+        {isLoggedIn && (
+          <NavLink to="/costs">
+            <Navbar.Link style={{ color: 'var(--secondary-color)' }} active>My costs</Navbar.Link>
+          </NavLink>
+        )}
+        <NavLink to="/about">
+          <Navbar.Link style={{ color: 'var(--secondary-color)' }} active>About</Navbar.Link>
+        </NavLink>
       </Navbar.Collapse>
     </Navbar>
   );
